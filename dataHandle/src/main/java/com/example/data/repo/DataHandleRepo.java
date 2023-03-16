f28a7b510ab8f1be4984861bf43a00af4f30150a
adhilabu <124715404+adhilabu@users.noreply.github.com>
2023-03-16 12:53:29 +0530

    Delete dataHandle directory

--- dataHandle/src/main/java/com/example/data/repo/DataHandleRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.data.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.data.bean.Data;
-
-public interface DataHandleRepo extends JpaRepository<Data, Long>{
-	
-}
